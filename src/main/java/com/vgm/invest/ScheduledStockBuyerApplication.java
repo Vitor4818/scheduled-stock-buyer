914c25e0b5ffa1b4d6135764010343b5b90706c3
unknown <vitorg.martins4818@gmail.com>
2026-03-11 16:17:04 -0300

    chore: initialize Spring Boot project using Spring Initializr

---
+++ src/main/java/com/vgm/invest/ScheduledStockBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.vgm.invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledStockBuyerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledStockBuyerApplication.class, args);
+	}
+
+}
